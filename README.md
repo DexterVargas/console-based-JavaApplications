c0f36654aa51a1e1c30eeb2e1d32787652d83f4b
dextervargasDev <dextervargas0912@gmail.com>
2025-09-13 22:17:47 +0800

    completed simple app.

--- README.md
+++ README.md
@@ -27,3 +27,75 @@ You will build a console-based Java application that allows users to manage thei
 - Author (String)
 - Year of Publication (int)
 - ISBN (String)
+
+### MAIN
+```
+________ Welcome to Book Management App ________
+1. Add Book
+2. Show All Books
+3. Delete Book
+4. Search Book by Title
+5. Exit
+Enter your choice: 
+```
+
+### Add
+```
+Enter your choice: 1
+| ************ ADD NEW BOOK *************** |
+Please enter the title: new book
+Please enter the author: John Doe
+Please enter the year published: 2025
+Please enter ISBN: 12345-678
+Book added successfully
+```
+
+### Show
+```
+Enter your choice: 2
+************************** LIST OF BOOKS ************************** 
+Title                          Author               Year  ISBN                
+------------------------------ -------------------- ----- --------------------
+Auto add book on startup       Test Constructor     2025  1236-4asd-23-09122025
+new book                       John Doe             2025  12345-678 
+```
+
+### Remove
+```
+Enter your choice: 3
+| ************ REMOVE a BOOK *************** |
+Enter ISBN of book to remove from your library: 12345-6789
+Book not found
+
+Enter your choice: 3
+| ************ REMOVE a BOOK *************** |
+Enter ISBN of book to remove from your library: 12345-678
+Book removed successfully
+```
+
+### Search 
+(includes books that contains the search param)
+```
+Enter your choice: 4
+Enter the title: book
+
+ ************************** SEARCH RESULT ***************************
+Title                          Author               Year  ISBN                
+------------------------------ -------------------- ----- --------------------
+2 book/s found successfully:
+Auto add book on startup       Test Constructor     2025  1236-4asd-23-09122025
+new book                       John Doe             2025  12345-678           
+-------------------------------Search result end--------------------------------
+```
+
+### Exit
+```
+Enter your choice: 5
+Exiting application...
+```
+
+### invalid choice
+```
+Enter your choice: asd
+'asd' is not a valid. Please enter valid number. 
+```
--- src/main/java/com/dexterv/Main.java
+++ src/main/java/com/dexterv/Main.java
@@ -3,8 +3,6 @@ package com.dexterv;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
     private static ArrayList<Book> books = new ArrayList<Book>();
@@ -14,21 +12,40 @@ public class Main {
         Book testBook = new Book("Auto add book on startup", "Test Constructor",2025, "1236-4asd-23-09122025");
         books.add(testBook);
 
-        showMain();
+        boolean running = true;
+        while (running) {
+            showMain();
+            String choice = scanner.nextLine();
+            // Check if the input is "e" to exit the application
+            if (choice.equalsIgnoreCase("e")) {
+                System.out.println("Exiting application...");
+                running = false;
+                continue; // Skip the rest of the loop and go to the next iteration
+            }
+            try {
 
-        int choice = scanner.nextInt();
-        System.out.println("You choose "+ choice);
-        scanner.nextLine();
-
-        addBook();
-
-        showBooks();
+            switch (Integer.parseInt(choice)) {
+                case 1 -> addBook();
+                case 2 -> showBooks();
+                case 3 -> removeBook();
+                case 4 -> searchBooks();
+                case 5 -> {
+                    System.out.println("Exiting application...");
+                    running = false;
+                }
+                default -> System.out.println("\nInvalid choice. Try again.\n");
+            }
+            } catch (NumberFormatException e) {
+                System.out.println("'" + choice + "' is not a valid. Please enter valid number. \n");
+            }
+        }
+        scanner.close();
     }
 
     private static void showMain(){
         System.out.println("________ Welcome to Book Management App ________");
         System.out.println("1. Add Book");
-        System.out.println("2. Update Book");
+        System.out.println("2. Show All Books");
         System.out.println("3. Delete Book");
         System.out.println("4. Search Book by Title");
         System.out.println("5. Exit");
@@ -55,10 +72,8 @@ public class Main {
 
     private static void showBooks(){
         System.out.println("************************** LIST OF BOOKS ************************** ");
-        String header = String.format("%-30s %-20s %-5s %-20s", "Title", "Author",  "Year", "ISBN");
-        String separator = String.format("%-30s %-20s %-5s %-20s", "------------------------------", "--------------------", "-----", "--------------------");
-        System.out.println(header);
-        System.out.println(separator);
+
+        displayInTableFormat();
 
         if(books.isEmpty()){
             System.out.println("No books in your library.");
@@ -67,6 +82,63 @@ public class Main {
 
         for(Book book : books){
             System.out.println(book.toString());
+        }
+    }
+
+    private static void displayInTableFormat() {
+        String header = String.format("%-30s %-20s %-5s %-20s", "Title", "Author",  "Year", "ISBN");
+        String separator = String.format("%-30s %-20s %-5s %-20s", "------------------------------", "--------------------", "-----", "--------------------");
+        System.out.println(header);
+        System.out.println(separator);
+    }
+
+    private static void searchBooks(){
+
+        System.out.print("Enter the title: ");
+        String title = scanner.nextLine().toLowerCase();
+
+        System.out.println("\n ************************** SEARCH RESULT ***************************");
+        displayInTableFormat();
+        boolean found = searchBookByTitle(title);
+
+
+        if(!found){
+            System.out.println("Title not found");
+        }
+    }
+
+    private static boolean searchBookByTitle(String title){
+        int counter = 0;
+
+        ArrayList<String> bookList = new ArrayList<>();
+
+        for(Book book : books){
+            if(book.getTitle().toLowerCase().contains(title)){
+                counter++;
+                bookList.add(book.toString());
+            }
+        }
+
+        if(counter > 0){
+            System.out.printf("%s book/s found successfully:\n",  counter);
+            for(String book : bookList)
+                System.out.println(book);
+        }
+
+        System.out.println("-------------------------------Search result end--------------------------------\n");
+        return true;
+    }
+
+    public static void removeBook(){
+        System.out.println("| ************ REMOVE a BOOK *************** |");
+        System.out.print("Enter ISBN of book to remove from your library: ");
+        String isbn = scanner.nextLine();
+        boolean removed = books.removeIf(book -> book.getIsbn().equals(isbn));
+
+        if(removed){
+            System.out.println("Book removed successfully");
+        } else {
+            System.out.println("Book not found");
         }
     }
 }
