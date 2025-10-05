7e6bab4e1b5c5b4cc15e5af51062a51090ee0cc3
dextervargasDev <dextervargas0912@gmail.com>
2025-10-05 18:52:55 +0800

    first commit

---
+++ src/main/java/dexterv/MyMain.java
@@ -0,0 +1,10 @@
+package main.java.dexterv;
+
+public class MyMain {
+
+    public static void main(String[] args) {
+        PhraseOMatic phrase = new PhraseOMatic();
+
+        phrase.exec();
+    }
+}
