81eb8260cbaebd10e5c7a657c03e8834fd7f8a44
dextervargasDev <dextervargas0912@gmail.com>
2025-10-21 07:09:53 +0800

    ship mode

--- src/main/java/dexterv/battleShip/BattleField.java
+++ src/main/java/dexterv/battleShip/BattleField.java
@@ -4,6 +4,10 @@ public class BattleField {
     private int areaX = 10;
     private int areaY = 10;
     private String[] str = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+
+    private int[] startAxis = {9,3};
+    private int len = 5;
+    private String direction;
     public void generateField() {
         for (int i = 0; i <= areaX; i++) {
             if (i == 0) {
@@ -12,9 +16,23 @@ public class BattleField {
                 System.out.print(i);
             }
 
-            for (int j = 0; j < areaY; j++) {
+            int increment = 0;
+            for (int j = 1; j <= areaY; j++) {
+
+
+
                 if (i == 0) {
-                    System.out.print("  " + str[j] + "  ");
+                    System.out.print("  " + j + "  ");
+                } else if (startAxis[0] == i) {
+                     if(startAxis[1] <= j && increment < len) {
+                         System.out.print(" [0] ");
+                         increment ++;
+
+
+                     }  else {
+                         System.out.print(" [ ] ");
+                     }
+
                 } else {
                     System.out.print(" [ ] ");
                 }
